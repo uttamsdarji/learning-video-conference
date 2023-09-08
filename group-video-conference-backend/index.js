f53cbd7da3fd6da756c2fdd4e9f958531303d554
Uttam <uttam@wareiq.com>
2023-09-09 01:52:12 +0530

    socket fix

--- group-video-conference-backend/index.js
+++ group-video-conference-backend/index.js
@@ -101,7 +101,7 @@ const credentials = { key: privateKey, cert: certificate, ca: ca };
 let httpServer = http.createServer(app);
 let httpsServer = https.createServer(credentials, app);
 
-const io = new Server(process.env.NODE_ENV == "production" ? httpServer : httpServer, {
+const io = new Server(process.env.NODE_ENV == "production" ? httpsServer : httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
